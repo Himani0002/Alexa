449776a5330454e6e4733b289297c44a6a6028ab
Himani0002 <himanigohil0002@gmail.com>
2024-07-29 15:58:45 +0530

    Delete Python directory

--- Python/oop.py
+++
@@ -1,8 +0,0 @@
-# Robo Speaker
-import os
-
-if __name__ == '__main__':
-    print("Welcome to RoboSpeaker 1.1 created by Himani")
-    x=input("Enter what you want me to speak: ")
-    command = f"say {x}"
-    os.system(command)
